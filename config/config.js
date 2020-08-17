fd1297c5524ab16b185cc0ec1745d6c2e1843874
JackDownload <kismocsy@gmail.com>
2020-08-17 09:17:06 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 const config = {
-    bucket_name: process.env.JAMSTACK_APP_BUCKET_SLUG || "Vassfazek",
-    bucket_slug: process.env.JAMSTACK_APP_BUCKET_SLUG || "vassfazek",
-    bucket_id: process.env.JAMSTACK_APP_BUCKET_ID || "5f3a28905abde300082e4a96",
-    read_key: process.env.JAMSTACK_APP_BUCKET_READ_KEY || "K8crdAJF2uxh5FsZjqGNIXGUMewH8j1VCXurrU8yYxhTC9QHqc",
-    write_key: process.env.JAMSTACK_APP_BUCKET_WRITE_KEY || "Ay1bBWNuYkogGJl0JNTieWueb4QuhlLAYu25VKziRiybsjEl7l",
+    bucket_name: process.env.JAMSTACK_APP_BUCKET_SLUG || "Vassfazék App",
+    bucket_slug: process.env.JAMSTACK_APP_BUCKET_SLUG || "vassfazek-app",
+    bucket_id: process.env.JAMSTACK_APP_BUCKET_ID || "5f3a2d8237f421000882e3f6",
+    read_key: process.env.JAMSTACK_APP_BUCKET_READ_KEY || "1IE5T6MZIX9Tvocyyo7RpEJFLwrL94znxvu3zWkp6YN2Cy3e6e",
+    write_key: process.env.JAMSTACK_APP_BUCKET_WRITE_KEY || "W7uhMsdxgEtGvurtHgsQtFbo1E5zUXvPtenNhTEB9BGNttAG9P",
     url: "https://api.cosmicjs.com/v1/"
   }
   export default config;
